5cebf60c5b78cae295ad3d410b7f9041035cb8d8
Yongfeng Chai <ychai@m3ts.com>
2023-03-30 14:47:09 -0500

    DispatchGroup

---
+++ APlayground.playground/Pages/Dispatch.xcplaygroundpage/Contents.swift
@@ -0,0 +1,21 @@
+//: [Previous](@previous)
+
+import Foundation
+
+//let group = DispatchGroup()
+//let cities = ["New York", "London", "Tokyo"]
+//var weatherData = [String: Weather]()
+//
+//for city in cities {
+//    group.enter()
+//    fetchWeather(for: city) { result in
+//        weatherData[city] = result
+//        group.leave()
+//    }
+//}
+//
+//group.notify(queue: .main) {
+//    updateUI(with: weatherData)
+//}
+
+//: [Next](@next)
